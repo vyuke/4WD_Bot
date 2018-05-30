a16d46183bdbcbf666be712e3d1dd77824a79ea8
yuke <1105313600@qq.com>
2018-05-30 18:03:18 +0800

    Add files via upload

---
+++ 4WD_Motor_Test/4WD_Motor_Test.ino
@@ -0,0 +1,50 @@
+#define A_1A 4 //A组电机正反转控制
+#define A_1B 5 //PWM
+#define B_1A 6 //PMW//B组电机正反转控制
+#define B_1B 7 //
+
+void motion(int speed_A, int speed_B) //电机速度控制函数。括号内分别为左右电机速度值，直接作用于L9110S电机驱动
+{
+  if (speed_A > 0) //范围-255～+255，正值为正转，负值为反转。
+  {
+    digitalWrite(A_1A, 0);
+    analogWrite(A_1B, speed_A); //PWM控制速度
+  }
+  else
+  {
+    digitalWrite(A_1A, 1);
+    analogWrite(A_1B, 255 + speed_A); //PWM控制速度
+  }
+  if (speed_B > 0)
+  {
+    analogWrite(B_1A, speed_B); //PWM控制速度
+    digitalWrite(B_1B, 0);
+  }
+
+  else
+  {
+    analogWrite(B_1A, 255 + speed_B); //PWM控制速度
+    digitalWrite(B_1B, 1);
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);    //打开串口设置波特率
+  pinMode(A_1A, OUTPUT); //配置电机输出IO口为输出
+  pinMode(A_1B, OUTPUT);
+  pinMode(B_1A, OUTPUT);
+  pinMode(B_1B, OUTPUT);
+}
+
+void loop()
+{
+  motion(255, 255); //全速前进
+  delay(1000);
+  motion(-255, -255); //全速后退
+  delay(1000);
+  motion(255, 0); //右转
+  delay(1000);
+  motion(0, 255); //左转
+  delay(1000);
+}
